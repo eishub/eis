34a1e401bfe748d830874298470ea6f7c85744cb
w-pasman <w-pasman@d1be27aa-122a-4001-a9ff-4e8566158a90>
2012-02-16 10:03:27 +0000

    added constructor to QueryException
    git-svn-id: https://svn.code.sf.net/p/apleis/code/trunk@100 d1be27aa-122a-4001-a9ff-4e8566158a90

--- eis-0.3/apleis/src/main/java/eis/exceptions/QueryException.java
+++ eis-0.3/apleis/src/main/java/eis/exceptions/QueryException.java
@@ -2,9 +2,13 @@ package eis.exceptions;
 
 /**
  * This exception is thrown when querying fails.
+ * 
  * @author tristanbehrens
- *
+ * @author W.Pasman 16feb2012 added constructor for convenience
+ * 
  */
 public class QueryException extends Exception {
-
+	public QueryException(String string) {
+		super(string);
+	}
 }
