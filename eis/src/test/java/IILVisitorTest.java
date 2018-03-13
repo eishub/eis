2487f81d9f3a026ab55ebd5f0c41a940dc6abd84
Wouter <w.pasman@tudelft.nl>
2018-03-13 10:21:40 +0100

    ENV-1335 split IILVisotorTest into separate tests of XML and Prolog visitor.

--- eis/src/test/java/IILVisitorTest.java
+++ eis/src/test/java/IILVisitorTest.java
@@ -1,6 +1,9 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.util.Vector;
+
+import org.junit.Test;
 
 import eis.iilang.IILElement;
 import eis.iilang.PrologVisitor;
@@ -9,46 +12,57 @@ import eis.iilang.XMLVisitor;
 public class IILVisitorTest {
 
 	/**
-	 * Tests both the XML- and Prolog-Visitor.
+	 * Test XML- Visitor.
 	 */
 	@SuppressWarnings("deprecation")
 	@Test
 	public void testXML() {
-		
+
 		Vector<IILElement> elements = IILExamples.correctExamples();
-		
+
 		// test to XML
-		for ( IILElement e : elements ) {
-			String str1,str2;
+		for (IILElement e : elements) {
+			String str1, str2;
 			str1 = e.toXML();
 			str2 = XMLVisitor.staticVisit(e);
-			String str1Clean = str1.replace(" ", "").replace("\n","").replace("\t","");
-			String str2Clean = str2.replace(" ", "").replace("\n","").replace("\t","");
+			String str1Clean = str1.replace(" ", "").replace("\n", "").replace("\t", "");
+			String str2Clean = str2.replace(" ", "").replace("\n", "").replace("\t", "");
 			boolean eq = str1Clean.equals(str2Clean);
-			if ( !eq ) {
-				System.out.println("toXML:\n" + str1); 
-				System.out.println("XMLVisitor:\n" + str2); 
+			if (!eq) {
+				System.out.println("toXML:\n" + str1);
+				System.out.println("XMLVisitor:\n" + str2);
 				fail("NOT EQUAL");
 			}
 			assertTrue(eq);
 		}
-		
+
+	}
+
+	/**
+	 * Test Prolog-Visitor.
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testProlog() {
+
+		Vector<IILElement> elements = IILExamples.correctExamples();
+
 		// test to Prolog
-		for ( IILElement e : elements ) {
-			String str1,str2;
+		for (IILElement e : elements) {
+			String str1, str2;
 			str1 = e.toProlog();
 			str2 = PrologVisitor.staticVisit(e);
-			String str1Clean = str1.replace(" ", "").replace("\n","").replace("\t","");
-			String str2Clean = str2.replace(" ", "").replace("\n","").replace("\t","");
+			String str1Clean = str1.replace(" ", "").replace("\n", "").replace("\t", "");
+			String str2Clean = str2.replace(" ", "").replace("\n", "").replace("\t", "");
 			boolean eq = str1Clean.equals(str2Clean);
-			if ( !eq ) {
-				System.out.println("toProlog:\n" + str1); 
-				System.out.println("PrologVisitor:\n" + str2); 
+			if (!eq) {
+				System.out.println("toProlog:\n" + str1);
+				System.out.println("PrologVisitor:\n" + str2);
 				fail("NOT EQUAL");
 			}
 			assertTrue(eq);
 		}
-		
+
 	}
 
 }
